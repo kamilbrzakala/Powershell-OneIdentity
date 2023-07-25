8959343bbe40b3fe6acb8203a9e2439aeeb2d5ac
kamilbrzakala <bzykson@gmail.com>
2023-07-25 12:59:42 +0200

    Create GetFileVersion.ps1

---
+++ GetFileVersion.ps1
@@ -0,0 +1,51 @@
+$parentFolder = "C:\Program Files\One Identity\One Identity Manager"
+$outputFile = "$env:USERPROFILE\Documents\FileVersions.csv"
+
+# ArrayList to store file version objects
+$fileVersions = New-Object System.Collections.ArrayList
+
+# Function to recursively get file versions
+function Get-FileVersion {
+    param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Alias("FullName")]
+        [string]$Path
+    )
+
+    if (Test-Path -Path $Path -PathType Leaf) {
+        $fileVersion = (Get-Item $Path).VersionInfo.FileVersion
+        $fileVersions.Add([PSCustomObject]@{
+            FileName = (Get-Item $Path).Name
+            FileVersion = $fileVersion
+        }) | Out-Null
+
+        Write-Output "$Path : $fileVersion"
+    }
+}
+
+# Recursive function to process folders
+function Process-Folder {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$FolderPath
+    )
+
+    # Process files in the current folder
+    Get-ChildItem -Path $FolderPath -File | ForEach-Object {
+        Get-FileVersion -Path $_.FullName
+    }
+
+    # Process subfolders
+    Get-ChildItem -Path $FolderPath -Directory | ForEach-Object {
+        Process-Folder -FolderPath $_.FullName
+    }
+}
+
+# Start processing from the parent folder
+Process-Folder -FolderPath $parentFolder
+
+# Export file versions to CSV
+$fileVersions | Export-Csv -Path $outputFile -NoTypeInformation -Encoding UTF8
+
+# Display success message
+Write-Host "File versions exported to: $outputFile"
